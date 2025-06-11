b3576042f0d604d0f1d30b525d1a3775f6762a2b
Anggaskyy <147676212+Anggaskyy@users.noreply.github.com>
2025-06-11 10:19:09 +0800

    Update Modul 1 DDL.sql
    Signed-off-by: Anggaskyy <147676212+Anggaskyy@users.noreply.github.com>

--- Modul-1-DDL/Modul 1 DDL.sql
+++ Modul-1-DDL/Modul 1 DDL.sql
@@ -4,29 +4,29 @@
 -- Kelas: Pendidikan Teknologi Informasi
 
 -- Membat Database 
-CREATE DATABASE pti_mart;
+CREATE DATABASE ptic_mart;
 
 -- Menggunakan Database
-USE pti_mart;
+USE ptic_mart;
 
 -- Membuat Tabel
-CREATE TABle Pelanggan(
-id_pelanggan INT PRIMARY KEY NOT NULL,
-nama_pelanggan VARCHAR (50),
-alamat VARCHAR (100),
-kota VARCHAR (20),
-no_tlp VARCHAR (15)
-);
-     
-CREATE TABLE produk(
-id_produk int PRIMARY KEY NOT NULL,
-nama_produk VARCHAR (50),
-kategori VARCHAR (20),
-harga int (17),
-stok int (10)
+create table ms_pelanggan_dqlab(
+id_pelanggan int PRIMARY KEY NOT NULL,
+nama_pelanggan VARCHAR(50),
+alamat VARCHAR(100),
+kota VARCHAR(15),
+no_tlp VARCHAR(15)
 );
 
--- membuat tabel dengan forign key
+create table ms_produk_dqlab(
+id_produk int primary key not null,
+nama_produk varchar(50),
+kategori varchar(50),
+harga int(15),
+stok int(15)
+);
+
+-- membuat tabel transaksi dengan forign key
 CREATE TABLE transaksi(
 id_transaksi int PRIMARY KEY NOT NULL,
 tgl_transaksi DATE,
@@ -38,24 +38,27 @@ FOREIGN KEY (id_produk) REFERENCES produk (id_produk),
 FOREIGN KEY (id_pelanggan) REFERENCES pelanggan (id_pelanggan)
 );
 
--- mengisi data dalam tabel
-INSERT INTO pelanggan(
-id_pelanggan, alamat, kota, no_tlp
-)VALUES
-(007," Skayzone", "Jl pemuda No.  255", "Mataram", "0897709");
+-- Mengisi data ke tabel pelanggan
+INSERT INTO pelanggan (id_pelanggan, nama_pelanggan, alamat, kota, no_tlp) 
+VALUES
+(211, "ucup", "Jl pemuda No. 567", "Mataram", "08266600724"),
+(007,"leviathan", "Jl pemuda No. 565", "Mataram", "08266600721"),
+(008,"obrock", "Jl suranadi No.19", "Mataram", "0829857464848"),
+(009,"skayzone", "Jl kuta No.26", "Mataram", "0829858464968"),
+(002,"bobrock", "Jl datok lopan No.10", "Mataram", "0829859464808");
 
 INSERT INTO produk(
 id_produk, nama_produk, kategori, harga, stok
 )VALUES
 (211, "mie", "makanan", "25.000", "2"),
-(007,"Leviathan", "Jl pemuda No. 565", "Mataram", "08266600721");
-(008,"bobrock", "Jl suranadi No.19", "Mataram", "0829857464848");
-(008,"skayzone", "Jl kuta No.26", "Mataram", "0829858464948");
-(008,"bobrock", "Jl datok lopan No.10", "Mataram", "0829859464808");
+(007, "sate", "makanan", "15.000", "1"),
+(008, "nasgor", "makanan", "35.000", "3"),
+(009, "mie ayam", "makanan", "25.000", "5"),
+(002, "kopi", "makanan", "10.000", "4");
 
--- cek apakah data ada
-SELECT FROM pelanggan;
-SELECT FROM produk;
+-- cek apakah data sudah ada
+SELECT * FROM pelanggan;
+SELECT * FROM produk;
 
 -- mengisi data pada tabel yang ada foreign key
 INSERT INTO transaksi
